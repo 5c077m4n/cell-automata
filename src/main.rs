97f3791fea448b9ae4bee34b37870341fb7679b0
Roee Shapira <ro33.sha@gmail.com>
2022-09-02 19:07:46 +0300

    Add logging

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
 mod lib;
 
-use bevy::prelude::*;
+use bevy::{
+	log::{Level, LogSettings},
+	prelude::*,
+};
 use lib::plugins::cell_auto::CellAutomataPlugin;
 
 fn main() {
 	App::new()
+		.insert_resource({
+			if cfg!(debug_assertions) {
+				LogSettings {
+					filter: "info,wgpu_core=warn,wgpu_hal=warn,minewars=debug".into(),
+					level: Level::DEBUG,
+				}
+			} else {
+				LogSettings {
+					filter: "warn".into(),
+					level: Level::WARN,
+				}
+			}
+		})
 		.add_plugins(DefaultPlugins)
 		.add_plugin(CellAutomataPlugin)
 		.run();
