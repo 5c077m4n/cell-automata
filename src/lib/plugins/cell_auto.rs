62fe28d7e5ecd16f91a7107d5a2685a040aedeba
Roee Shapira <ro33.sha@gmail.com>
2022-09-03 22:12:01 +0300

    Fix startup system

--- src/lib/plugins/cell_auto.rs
+++ src/lib/plugins/cell_auto.rs
@@ -8,45 +8,29 @@ struct Location(usize, usize, usize);
 #[derive(Component, Default, Debug)]
 struct State(usize);
 
-fn init_cubes(mut commands: Commands) {
-	trace!("Creating cube objects");
-	for i in 0..1000 {
-		commands
-			.spawn()
-			.insert(Cube)
-			.insert(Location(i % 10, i % 10, i % 10))
-			.insert(State(5));
-	}
-}
-fn move_cubes(
-	mut cubes: Query<&mut Transform, With<Cube>>,
-	mut timer: ResMut<StateTimer>,
-	time: Res<Time>,
-) {
-	if timer.0.tick(time.delta()).just_finished() {
-		trace!("Moving cubes");
-		for mut transform in &mut cubes {
-			let forward = transform.forward();
-			transform.translation += forward * time.delta_seconds();
-		}
+fn move_cubes(mut cubes: Query<&mut Transform, With<Cube>>, time: Res<Time>) {
+	trace!("Moving cubes");
+	for mut transform in &mut cubes {
+		let direction = transform.local_x();
+		transform.translation += direction * time.delta_seconds();
 	}
 }
 
-fn draw(
+fn setup(
 	mut commands: Commands,
 	mut meshes: ResMut<Assets<Mesh>>,
 	mut materials: ResMut<Assets<StandardMaterial>>,
-	cubes: Query<(&Location, &State), With<Cube>>,
 ) {
-	for (location, _state) in cubes.iter() {
-		let Location(x, y, z) = location;
-
-		commands.spawn_bundle(PbrBundle {
-			mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-			material: materials.add(Color::rgba(1.0, 1.0, 0.5, 0.75).into()),
-			transform: Transform::from_xyz(*x as f32, *y as f32, *z as f32),
-			..default()
-		});
+	for i in -5..5 {
+		commands
+			.spawn_bundle(PbrBundle {
+				mesh: meshes.add(Mesh::from(shape::Cube { size: 0.1 })),
+				material: materials.add(Color::rgba(1.0, 1.0, 0.5, 0.75).into()),
+				transform: Transform::from_xyz((i % 10) as f32, (i % 10) as f32, (i % 10) as f32),
+				..default()
+			})
+			.insert(Cube)
+			.insert(State(5));
 	}
 	commands.spawn_bundle(Camera3dBundle {
 		transform: Transform::from_xyz(0.0, 10.0, 20.0).looking_at(Vec3::ZERO, Vec3::Y),
@@ -62,8 +46,7 @@ pub struct CellAutomataPlugin;
 impl Plugin for CellAutomataPlugin {
 	fn build(&self, app: &mut App) {
 		app.insert_resource(StateTimer(Timer::from_seconds(1., true)))
-			.add_startup_system(init_cubes)
-			.add_startup_system(draw)
+			.add_startup_system(setup)
 			.add_system(move_cubes);
 	}
 }
