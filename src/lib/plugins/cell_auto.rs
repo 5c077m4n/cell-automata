af9720ffbf3eea6c625076eb93b84dc5666ffbbc
Roee Shapira <ro33.sha@gmail.com>
2022-09-04 23:12:42 +0300

    Add despawn on decayed cubes

--- src/lib/plugins/cell_auto.rs
+++ src/lib/plugins/cell_auto.rs
@@ -1,18 +1,39 @@
 use super::super::resources::state_timer::StateTimer;
 use bevy::prelude::*;
 
-#[derive(Component, Default, Debug)]
+#[derive(Component)]
 struct Cube;
 #[derive(Component, Default, Debug)]
-struct Location(usize, usize, usize);
+struct Location(Vec3);
 #[derive(Component, Default, Debug)]
-struct State(usize);
+struct State {
+	decay: bool,
+	count: usize,
+}
 
-fn move_cubes(mut cubes: Query<&mut Transform, With<Cube>>, time: Res<Time>) {
-	trace!("Moving cubes");
-	for mut transform in &mut cubes {
+fn move_cubes(mut query: Query<&mut Transform, With<Cube>>, time: Res<Time>) {
+	for mut transform in query.iter_mut() {
 		let direction = transform.local_x();
 		transform.translation += direction * time.delta_seconds();
+	}
+}
+fn decay_step(
+	mut commands: Commands,
+	mut query: Query<(Entity, &mut State), With<Cube>>,
+	time: Res<Time>,
+	mut timer: ResMut<StateTimer>,
+) {
+	if timer.0.tick(time.delta()).just_finished() {
+		for (entity, mut state) in query.iter_mut() {
+			if state.decay {
+				if let Some(n) = state.count.checked_sub(1) {
+					state.count = n;
+				}
+				if state.count == 0 {
+					commands.entity(entity).despawn();
+				}
+			}
+		}
 	}
 }
 
@@ -29,8 +50,13 @@ fn setup(
 				transform: Transform::from_xyz((i % 10) as f32, (i % 10) as f32, (i % 10) as f32),
 				..default()
 			})
-			.insert(Cube)
-			.insert(State(5));
+			.insert_bundle((
+				Cube,
+				State {
+					decay: i % 3 == 0,
+					count: 5,
+				},
+			));
 	}
 	commands.spawn_bundle(Camera3dBundle {
 		transform: Transform::from_xyz(0.0, 10.0, 20.0).looking_at(Vec3::ZERO, Vec3::Y),
@@ -47,6 +73,7 @@ impl Plugin for CellAutomataPlugin {
 	fn build(&self, app: &mut App) {
 		app.insert_resource(StateTimer(Timer::from_seconds(1., true)))
 			.add_startup_system(setup)
-			.add_system(move_cubes);
+			.add_system(move_cubes)
+			.add_system(decay_step);
 	}
 }
