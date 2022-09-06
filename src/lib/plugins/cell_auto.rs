61fa93c39631ba59504af505b8be5693d07e84ef
Roee Shapira <ro33.sha@gmail.com>
2022-09-06 23:59:36 +0300

    Add neighbour detection to start decay

--- src/lib/plugins/cell_auto.rs
+++ src/lib/plugins/cell_auto.rs
@@ -3,6 +3,7 @@ use bevy::prelude::*;
 
 const N_CUBES: usize = 10;
 const CUBE_SIZE: f32 = 2.0;
+const STATE_COUNT: usize = 5;
 
 #[derive(Component)]
 struct Cube;
@@ -12,28 +13,65 @@ struct State {
 	count: usize,
 }
 
-fn move_cubes(mut cube_query: Query<&mut Transform, With<Cube>>, time: Res<Time>) {
-	for mut transform in cube_query.iter_mut() {
-		let direction = transform.local_x();
-		transform.translation += direction * time.delta_seconds();
+fn move_cubes(
+	mut cube_query: Query<(&mut Transform, &GlobalTransform, &mut State), With<Cube>>,
+	time: Res<Time>,
+	mut timer: ResMut<DecayTimer>,
+) {
+	if timer.0.tick(time.delta()).just_finished() {
+		let cubes: Vec<_> = cube_query.iter_mut().collect();
+
+		let mut neighbour_count: usize = 0;
+		for (trns, global_trns, _state) in &cubes {
+			let cubes_clone: Vec<_> = cubes.iter().collect();
+
+			let mut translation = trns.translation;
+			let global_translation = global_trns.translation();
+
+			for (curr_trns, curr_global_trns, _) in &cubes_clone {
+				let curr_global_translation = curr_global_trns.translation();
+				let (x, y, z) = (
+					curr_trns.local_x(),
+					curr_trns.local_y(),
+					curr_trns.local_z(),
+				);
+
+				if curr_global_translation + x == global_translation {
+					neighbour_count += 1;
+				}
+				if curr_global_translation - x == global_translation {
+					neighbour_count += 1;
+				}
+				if curr_global_translation + y == global_translation {
+					neighbour_count += 1;
+				}
+				if curr_global_translation - y == global_translation {
+					neighbour_count += 1;
+				}
+				if curr_global_translation + z == global_translation {
+					neighbour_count += 1;
+				}
+				if curr_global_translation - z == global_translation {
+					neighbour_count += 1;
+				}
+				info!("{}", neighbour_count);
+			}
+
+			if neighbour_count == 4 {
+				let direction = trns.local_x();
+				translation += direction * time.delta_seconds();
+			}
+		}
 	}
 }
 fn decay_step(
 	mut commands: Commands,
-	mut cube_query: Query<
-		(
-			Entity,
-			&mut State,
-			&mut ComputedVisibility,
-			Option<&mut UiColor>,
-		),
-		With<Cube>,
-	>,
+	mut cube_query: Query<(Entity, &mut State, &ComputedVisibility), With<Cube>>,
 	mut timer: ResMut<DecayTimer>,
 	time: Res<Time>,
 ) {
 	if timer.0.tick(time.delta()).just_finished() {
-		for (entity, mut state, comp_vis, ui_color) in cube_query.iter_mut() {
+		for (entity, mut state, comp_vis) in cube_query.iter_mut() {
 			if !comp_vis.is_visible() {
 				commands.entity(entity).despawn();
 			} else if state.decay {
@@ -42,8 +80,6 @@ fn decay_step(
 				}
 				if state.count == 0 {
 					commands.entity(entity).despawn();
-				} else if let Some(mut ui_color) = ui_color {
-					ui_color.0.set_a(1.0 - (1.0 - state.count as f32));
 				}
 			}
 		}
@@ -60,7 +96,7 @@ fn setup(
 		commands
 			.spawn_bundle(PbrBundle {
 				mesh: meshes.add(Mesh::from(shape::Cube { size: CUBE_SIZE })),
-				material: materials.add(Color::rgba(1.0, 1.0, 0.5, 1.0).into()),
+				material: materials.add(Color::ANTIQUE_WHITE.into()),
 				transform: Transform::from_xyz(
 					(i % 10) as f32 * CUBE_SIZE,
 					(i % 10) as f32 * CUBE_SIZE,
@@ -71,8 +107,8 @@ fn setup(
 			.insert_bundle((
 				Cube,
 				State {
-					decay: i % 2 == 0,
-					count: 5,
+					decay: false,
+					count: STATE_COUNT,
 				},
 			));
 	}
