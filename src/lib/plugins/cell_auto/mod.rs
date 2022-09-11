ce4f0db4b91e0b98e8bb9ba76102b4ff58ef0f3d
Roee Shapira <ro33.sha@gmail.com>
2022-09-12 00:08:27 +0300

    Make cubes children of plane

--- src/lib/plugins/cell_auto/mod.rs
+++ src/lib/plugins/cell_auto/mod.rs
@@ -16,21 +16,28 @@ fn setup(
 	mut meshes: ResMut<Assets<Mesh>>,
 	mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-	let half_n_cubes: isize = N_CUBES as isize / 2;
-	for i in -half_n_cubes..half_n_cubes {
-		commands
-			.spawn_bundle(PbrBundle {
-				mesh: meshes.add(Mesh::from(shape::Cube { size: CUBE_SIZE })),
-				material: materials.add(Color::ANTIQUE_WHITE.into()),
-				transform: Transform::from_xyz(
-					(i % 10) as f32 * CUBE_SIZE,
-					(i % 10) as f32 * CUBE_SIZE,
-					0.0 * CUBE_SIZE,
-				),
-				..default()
-			})
-			.insert_bundle((Cube, State::new(STATE_COUNT)));
-	}
+	commands
+		.spawn_bundle(PbrBundle {
+			mesh: meshes.add(Mesh::from(shape::Plane { size: 10.0 })),
+			material: materials.add(Color::NONE.into()),
+			..default()
+		})
+		.with_children(|p| {
+			let half_n_cubes: isize = N_CUBES as isize / 2;
+			for i in -half_n_cubes..half_n_cubes {
+				p.spawn_bundle(PbrBundle {
+					mesh: meshes.add(Mesh::from(shape::Cube { size: CUBE_SIZE })),
+					material: materials.add(Color::ANTIQUE_WHITE.into()),
+					transform: Transform::from_xyz(
+						(i % 10) as f32 * CUBE_SIZE,
+						(i % 10) as f32 * CUBE_SIZE,
+						0.0 * CUBE_SIZE,
+					),
+					..default()
+				})
+				.insert_bundle((Cube, State::new(STATE_COUNT)));
+			}
+		});
 	commands.spawn_bundle(Camera3dBundle {
 		transform: Transform::from_xyz(10.0, 10.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
 		..default()
